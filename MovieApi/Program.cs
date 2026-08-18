529401ca0ca8032a5f9b4e3b7dc6956037b1be45
Xgor <real.xgor@hotmail.com>
2026-08-18 16:28:08 +0200

    Added movies and MVC code

--- MovieApi/Program.cs
+++ MovieApi/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -13,6 +15,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
 
 var summaries = new[]
 {
