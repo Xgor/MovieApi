d8b3cf17a4a3479c80ca1caaba4bb1c03abd3dee
Xgor <real.xgor@hotmail.com>
2026-08-28 14:21:56 +0200

    All basic endpoints done

--- MovieApi/Controllers/MoviesController.cs
+++ MovieApi/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MovieApi.Data;
+using MovieApi.Models;
 using MovieApi.Models.DTOs;
 
 namespace MovieApi.Controllers;
@@ -18,19 +19,55 @@ public class MoviesController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MovieDto>>> GetMovies()
     {
-        var movies = await _context.Movie.ToListAsync();
+        var movies = await _context.Movie.Select(m => new MovieDto()
+        {
+            Description = m.Description,
+            Director = m.Director,
+            DurationMinutes = m.DurationMinutes,
+            Genre = m.Genre,
+            Id = m.Id,
+            Rating = m.Rating,
+            ReleaseYear = m.ReleaseYear,
+            Title = m.Title
+        }).ToListAsync();
         return Ok(movies);
     }
 
     [HttpGet("{id:int}")]
     public async Task<ActionResult<MovieDto>> GetMovie(int id)
     {
-        return NotFound();
+        var movie = await _context.Movie.Select(m => new MovieDto()
+        {
+            Description = m.Description,
+            Director = m.Director,
+            DurationMinutes = m.DurationMinutes,
+            Genre = m.Genre,
+            Id = m.Id,
+            Rating = m.Rating,
+            ReleaseYear = m.ReleaseYear,
+            Title = m.Title
+        }).FirstOrDefaultAsync(m => m.Id == id);
+        if (movie == null)
+            return NotFound();
+        return Ok(movie);
     }
 
     [HttpPost]
     public async Task<ActionResult<MovieDto>> CreateMovie(CreateMovieDto dto)
     {
-        return NotFound();
+        var movie = new Movie()
+        {
+            Description = dto.Description,
+            Director = dto.Director,
+            DurationMinutes = dto.DurationMinutes,
+            Rating = dto.Rating,
+            Title = dto.Title,
+            ReleaseYear = dto.ReleaseYear,
+            Genre = dto.Genre,
+        };
+
+        _context.Movie.Add(movie);
+        await _context.SaveChangesAsync();
+        return Ok(movie);
     }
 }
